1c8738dcf3e7bc47a016ea7bd12702810be7d369
Dsschieber <dougschieberanimation@gmail.com>
2016-10-27 13:47:52 -0500

    Update IKFKutli.py

--- FKIKBuilder/IKFKutli.py
+++ FKIKBuilder/IKFKutli.py
@@ -16,4 +16,13 @@ class IKFK(self):
     cmds.move( 3, 0, 0, varLoc1, relative=True, objectSpace=True, worldSpaceDistance=True)
     cmds.move( 1.5, 0, 1, varLoc2, relative=True, objectSpace=True, worldSpaceDistance=True)
     cmds.move( 0, 0, 0, varLoc3, relative=True, objectSpace=True, worldSpaceDistance=True)
-	
+  def createObjects():
+    ikObjects = IkFkBuilder(prefix = 'leg', joint1 = 'limb_loc_1', joint2 =  'limb_loc_2', joint3 =  'limb_loc_3', twistAxis = 'x')
+    ikObjects.makeLimb("ik")
+    ikObjects.createIkjointChain()
+    ikObjects.doIt()
+    fkObjects = IkFkBuilder(prefix = 'leg', joint1 = 'limb_loc_1', joint2 =  'limb_loc_2', joint3 =  'limb_loc_3', twistAxis = 'x')
+    fkObjects.makeLimb("fk")
+    #fkObjects.to_String()
+    fkObjects.createFKjointChain()
+
