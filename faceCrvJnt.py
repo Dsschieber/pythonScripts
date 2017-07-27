10c61a36c05623b4306dcba31f581242e4402e6e
Dsschieber <dougschieberanimation@gmail.com>
2017-07-27 16:26:30 -0500

    Fully Functional
    select a curve and it should work.
    will need to provide an upvector and locator encase you want a center
    pivot

--- faceCrvJnt.py
+++ faceCrvJnt.py
@@ -1,9 +1,22 @@
+'''
+Author @Doug Schieber
+Email DougSchieberAnimation@gmail.com
 
-#FIRST
+
+Functions
+-Takes edge information and creates an edge with joints and controls attached
+
+Future Additions
+-attach to nurbs surface
+-use center locator pivot
+-create upVec and centerLocator
+-attach objects to parent (say a jaw joint or etc)
+
+'''
+
 
 from maya import cmds, OpenMaya
 import maya.mel as mel
-#for each vertex it will create a joint 
 
 
 '''
@@ -120,6 +133,34 @@ def getDagPath( objectName):
         selectionList.getDagPath(0, oNode)
         return oNode
 '''
+creates controls from object position
+
+
+'''
+
+def faceCtrlCreate(prefix = '', objects = []):
+	ctrls = []
+	for stuff in objects:
+		# control
+		ctrl = mel.eval('curve -d 1 -p 0 1 0 -p 0 0.987688 -0.156435 -p 0 0.951057 -0.309017 -p 0 0.891007 -0.453991 -p 0 0.809017 -0.587786 -p 0 0.707107 -0.707107 -p 0 0.587785 -0.809017 -p 0 0.453991 -0.891007 -p 0 0.309017 -0.951057 -p 0 0.156434 -0.987689 -p 0 0 -1 -p 0 -0.156434 -0.987689 -p 0 -0.309017 -0.951057 -p 0 -0.453991 -0.891007 -p 0 -0.587785 -0.809017 -p 0 -0.707107 -0.707107 -p 0 -0.809017 -0.587786 -p 0 -0.891007 -0.453991 -p 0 -0.951057 -0.309017 -p 0 -0.987688 -0.156435 -p 0 -1 0 -p -4.66211e-09 -0.987688 0.156434 -p -9.20942e-09 -0.951057 0.309017 -p -1.353e-08 -0.891007 0.453991 -p -1.75174e-08 -0.809017 0.587785 -p -2.10734e-08 -0.707107 0.707107 -p -2.41106e-08 -0.587785 0.809017 -p -2.65541e-08 -0.453991 0.891007 -p -2.83437e-08 -0.309017 0.951057 -p -2.94354e-08 -0.156434 0.987688 -p -2.98023e-08 0 1 -p -2.94354e-08 0.156434 0.987688 -p -2.83437e-08 0.309017 0.951057 -p -2.65541e-08 0.453991 0.891007 -p -2.41106e-08 0.587785 0.809017 -p -2.10734e-08 0.707107 0.707107 -p -1.75174e-08 0.809017 0.587785 -p -1.353e-08 0.891007 0.453991 -p -9.20942e-09 0.951057 0.309017 -p -4.66211e-09 0.987688 0.156434 -p 0 1 0 -p -0.156435 0.987688 0 -p -0.309017 0.951057 0 -p -0.453991 0.891007 0 -p -0.587785 0.809017 0 -p -0.707107 0.707107 0 -p -0.809017 0.587785 0 -p -0.891007 0.453991 0 -p -0.951057 0.309017 0 -p -0.987689 0.156434 0 -p -1 0 0 -p -0.987689 -0.156434 0 -p -0.951057 -0.309017 0 -p -0.891007 -0.453991 0 -p -0.809017 -0.587785 0 -p -0.707107 -0.707107 0 -p -0.587785 -0.809017 0 -p -0.453991 -0.891007 0 -p -0.309017 -0.951057 0 -p -0.156435 -0.987688 0 -p 0 -1 0 -p 0.156434 -0.987688 0 -p 0.309017 -0.951057 0 -p 0.453991 -0.891007 0 -p 0.587785 -0.809017 0 -p 0.707107 -0.707107 0 -p 0.809017 -0.587785 0 -p 0.891006 -0.453991 0 -p 0.951057 -0.309017 0 -p 0.987688 -0.156434 0 -p 1 0 0 -p 0.951057 0 -0.309017 -p 0.809018 0 -0.587786 -p 0.587786 0 -0.809017 -p 0.309017 0 -0.951057 -p 0 0 -1 -p -0.309017 0 -0.951057 -p -0.587785 0 -0.809017 -p -0.809017 0 -0.587785 -p -0.951057 0 -0.309017 -p -1 0 0 -p -0.951057 0 0.309017 -p -0.809017 0 0.587785 -p -0.587785 0 0.809017 -p -0.309017 0 0.951057 -p -2.98023e-08 0 1 -p 0.309017 0 0.951057 -p 0.587785 0 0.809017 -p 0.809017 0 0.587785 -p 0.951057 0 0.309017 -p 1 0 0 -p 0.987688 0.156434 0 -p 0.951057 0.309017 0 -p 0.891006 0.453991 0 -p 0.809017 0.587785 0 -p 0.707107 0.707107 0 -p 0.587785 0.809017 0 -p 0.453991 0.891007 0 -p 0.309017 0.951057 0 -p 0.156434 0.987688 0 -p 0 1 0 ;')
+		cmds.parentConstraint(stuff,ctrl,mo=False)
+		cmds.delete(ctrl,cn=True)
+		newName = cmds.rename(prefix+'_ctrl_#')
+		ctrls.append(newName)
+	return ctrls
+'''
+
+creates center locator and upVec locator
+
+
+'''
+
+
+def createLocs(prefix = ''):
+	pass
+
+
+'''
 for grouping controllers so they have offset
 
 '''
@@ -170,8 +211,20 @@ instructions: select an edge and make some nice face controls
 
 possible create a nurbs surface for sliding? 
 
+Everything below should be broken up into functions for organization. 
+
+-center needs proper scaling.
+-aim joints need to be named.
+-locator group cannot be grouped under scale group
+-parent everything under center locator for scaling
+
 '''
 
+#prefix
+prefix = 'test'
+
+#hierarchy groups
+hierarchyGrp = []
 
 #edge is selected
 edges = cmds.ls(sl=True)
@@ -181,60 +234,105 @@ vertices = cmds.polyListComponentConversion(edges, fe=True, tv=True)
 
 #create a curve from edge
 curves = curveFromVertPositions(edges)
-curveObj = cmds.rename( curves, "curve_hiRez_crv")
+curveObj = cmds.rename( curves, prefix + "_hiRez_crv")
 cmds.select(vertices)
 cmds.delete(curveObj, ch=True)
 
 #limit the amount of joints for hiRez topo
 limitHiRexJoints = 1
-jointsObjects = []
+tmpJNTs = []
 
 #var determines if there will be a aim constraint based crv or a parent based. 
 #center would best be used for eyes where a center can easily be obtained. 
-findYourCenter = False
+findYourCenter = True
 upVecForCenter = 'upVec'
 centerLocator = 'obj_loc'
-jointsObjects = first(center = centerLocator, fromCenter = findYourCenter, step = limitHiRexJoints)
-jntParent = cmds.listRelatives(jointsObjects, p=True)
-#rebuildCurve -ch 1 -rpo 0 -rt 0 -end 1 -kr 0 -kcp 0 -kep 1 -kt 0 -s 3 -d 3 -tol 0.01 "curve_hiRez_crv";
+tmpJNTs = first(center = centerLocator, fromCenter = findYourCenter, step = limitHiRexJoints)
+bindJNTs = []
+num = len(tmpJNTs)
+rnTempJnts = []
+
+#rebuild list for naming
+for each in tmpJNTs:
+	tEmPbLaH = cmds.listRelatives(each, p=True)
+	if (findYourCenter == True):
+		cmds.rename(tEmPbLaH, prefix + '_aimJNT_' + str(len(tmpJNTs) - num))
+	bindJNTs.append(cmds.rename(each, prefix + '_bnJNT_' + str(len(tmpJNTs) - num)))
+	num = num - 1
+
+jntParent = cmds.listRelatives(bindJNTs, p=True)
 
 #attach to locs or attach to nulls? 
 #provide second loc for up vector
-locators = second(upVector = upVecForCenter, fromCenter = findYourCenter, objects = jointsObjects)
+locators = second(upVector = upVecForCenter, fromCenter = findYourCenter, objects = bindJNTs)
+pinnedLocs = []
+num = len(locators)
+
+for each in locators:
+	pinnedLocs.append(cmds.rename(each, prefix + '_loc_' + str(len(locators) - num)))
+	num = num - 1
 #sel = cmds.ls(sl=True)
 #cmds.select(objects)
-third(curveObj, locators)
+third(curveObj, pinnedLocs)
 
 #clean up, vars for groups would be nice
 if (findYourCenter == False):
-	for i in range(len(jointsObjects)):
-		cmds.parent(jointsObjects[i], locators[i])
-	cmds.group(locators, n='locator_grp')
+	for i in range(len(bindJNTs)):
+		cmds.parent(bindJNTs[i], pinnedLocs[i])
+	hierarchyGrp.append(cmds.group(pinnedLocs, n=prefix + 'locator_grp'))
 	
 else:
 	#parent under center locator?
-	cmds.group(jntParent, n='joint_grp')
-	cmds.group(locators, n='locator_grp')
+	hierarchyGrp.append(cmds.group(jntParent, n=prefix + 'joint_grp'))
+	hierarchyGrp.append(cmds.group(pinnedLocs, n=prefix +'locator_grp'))
 	print('parent not needed')
 
 #create a lowRez Curve for controllers
 tempCrv = cmds.rebuildCurve(curveObj, ch=1, rpo=0,rt=0,end=1, kr=0,kcp=0,kep=1,kt=0,s=2,d=3,tol=0.01)
 cmds.delete(tempCrv, ch=True)
-lowRezCurve = cmds.rename(tempCrv[0],"curve_loRez_crv")
-cmds.wire( curveObj , w = lowRezCurve, gw= False, en=1.000000, ce=0.000000, li=0.000000, dds=[(0, 100)], n='lowRez_wire')
+lowRezCurve = cmds.rename(tempCrv[0],prefix + "curve_loRez_crv")
+cmds.wire( curveObj , w = lowRezCurve, gw= False, en=1.000000, ce=0.000000, li=0.000000, dds=[(0, 100)], n=prefix + 'lowRez_wire')
 
 #get the low resolution curves cvs
 cmds.select(lowRezCurve + '.cv[0:4]')
-ctrlJoint = first()
+ctrlTmpJoints = first()
+num = len(ctrlTmpJoints)
+ctrlJNTs = []
+
+#rename control joints
+for each in ctrlTmpJoints:
+	ctrlJNTs.append(cmds.rename(each, prefix + '_ctrlJNT_' + str(len(ctrlTmpJoints) - num)))
+	num = num - 1
 
 #skin Control joints to lowRezCurve 
+ctrlJNTgrp = groupSpecial(ctrlJNTs)
+hierarchyGrp.append(cmds.group(ctrlJNTgrp, n=prefix +'_ctrlJNTs_grp'))
+cmds.skinCluster(ctrlJNTs, lowRezCurve, dr=4.0, nw=1, mi=1, n = prefix + 'skinClstr')
 
 #create Controls for CtrlJnts with offset
+ctrls = faceCtrlCreate(prefix = prefix, objects = ctrlJNTgrp)
+ctrlNulls = groupSpecial(ctrls)
+hierarchyGrp.append(cmds.group(ctrlNulls, n=prefix+'_ctrl_grp'))
+#color variable
+colorNum = 14
 
-#grp ctrls with an offset
+#lock and hide controls, parent to ctrl jnts
+for i in range(len(ctrls)):
+	cmds.setAttr(ctrls[i]+'.sx', l=True, k=False, cb=False)
+	cmds.setAttr(ctrls[i]+'.sy', l=True, k=False, cb=False)
+	cmds.setAttr(ctrls[i]+'.sz', l=True, k=False, cb=False)
+	cmds.setAttr(ctrls[i]+'.v', l=True, k=False, cb=False)
+	cmds.parentConstraint(ctrls[i], ctrlJNTgrp[i])
+	cmds.setAttr(ctrls[i]+".overrideEnabled",1)
+	cmds.setAttr(ctrls[i]+".overrideColor",colorNum)
 
-#parent ctrl joints
 
 #group everything to where it will need to be grouped (for scaling and organization)
+if (findYourCenter == False):
+	scaleGrp = cmds.group(hierarchyGrp[1], hierarchyGrp[2], n=prefix+'_scale_grp')
+	for each in bindJNTs:
+		cmds.scaleConstraint(scaleGrp, each)
+else:
+	cmds.parent(hierarchyGrp[0], hierarchyGrp[2], hierarchyGrp[3], upVecForCenter, centerLocator)
 
 #voila it is done
